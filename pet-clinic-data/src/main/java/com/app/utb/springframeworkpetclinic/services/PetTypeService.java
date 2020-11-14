70200ac7cce83003337e40a90d12df08510045f1
SyedIftekharuddin <syedy636@gmail.com>
2020-11-14 21:44:16 +0100

    Create PetType Map Service. Closes #31

---
+++ pet-clinic-data/src/main/java/com/app/utb/springframeworkpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.app.utb.springframeworkpetclinic.services;
+
+import com.app.utb.springframeworkpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
